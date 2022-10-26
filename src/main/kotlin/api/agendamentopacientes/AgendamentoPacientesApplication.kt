a9b6e9e47d3a4b1eab5bcb2d56006623736e03ff
Dennis Castro <d3nnis_castro@hotmail.com>
2022-10-25 22:11:57 -0300

    commit inicial

---
+++ src/main/kotlin/api/agendamentopacientes/AgendamentoPacientesApplication.kt
@@ -0,0 +1,11 @@
+package api.agendamentopacientes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgendamentoPacientesApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgendamentoPacientesApplication>(*args)
+}
---
+++ src/test/kotlin/api/agendamentopacientes/AgendamentoPacientesApplicationTests.kt
@@ -0,0 +1,13 @@
+package api.agendamentopacientes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgendamentoPacientesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
